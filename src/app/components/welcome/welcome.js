3e84cc3700c4cd8a89b669d67198ce2230dd99c2
ariellenamelist <arielle.namelist@gmail.com>
2025-05-27 22:21:30 +0530

    Update welcome.js

--- src/app/components/welcome/welcome.js
+++ src/app/components/welcome/welcome.js
@@ -110,7 +110,7 @@ export default function Welcome() {
 					</div>					
 					<br/>	
 					<div className="text-center anc-tab">
-						<a className="elementor-button elementor-button-link elementor-size-sm elementor-animation-pulse-grow" href="#">
+						<a className="elementor-button elementor-button-link elementor-size-sm elementor-animation-pulse-grow" href="https://wa.me/918441068489">
 							<span className="elementor-button-content-wrapper">
 								
 								<span className="elementor-button-text">Get CricBet99 Login ID</span>
