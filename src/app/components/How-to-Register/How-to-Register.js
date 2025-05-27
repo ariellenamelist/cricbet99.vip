8f61fa6fcb994e5a8f346eabf2baa773b16c45ab
ariellenamelist <arielle.namelist@gmail.com>
2025-05-27 22:18:20 +0530

    Update How-to-Register.js

--- src/app/components/How-to-Register/How-to-Register.js
+++ src/app/components/How-to-Register/How-to-Register.js
@@ -39,7 +39,7 @@ export default function HowtoRegister () {
     <div className="text-left anc-tab">
       <a
         className="elementor-button elementor-button-link elementor-size-sm elementor-animation-pulse-grow"
-        href="#"
+        href="https://wa.me/918441068489"
       >
         <span className="elementor-button-content-wrapper">
           <span className="elementor-button-text">Let's CricBet99 Sign-Up Now</span>
@@ -79,7 +79,7 @@ export default function HowtoRegister () {
                 <div className="text-left anc-tab">
                 <a
                     className="elementor-button elementor-button-link elementor-size-sm elementor-animation-pulse-grow"
-                    href="#"
+                    href="https://wa.me/918441068489"
                 >
                     <span className="elementor-button-content-wrapper">
                     <span className="elementor-button-text">Get CricBet99 Login ID</span>
@@ -118,7 +118,7 @@ export default function HowtoRegister () {
                 <div className="text-left anc-tab">
                 <a
                     className="elementor-button elementor-button-link elementor-size-sm elementor-animation-pulse-grow"
-                    href="#"
+                    href="https://wa.me/918441068489"
                 >
                     <span className="elementor-button-content-wrapper">
                     <span className="elementor-button-text">Get CricBet99 APK Download Now</span>
