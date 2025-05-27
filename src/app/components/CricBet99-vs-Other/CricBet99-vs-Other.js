5afa6420bb29820e9189c8567b82681d6b2c94b1
ariellenamelist <arielle.namelist@gmail.com>
2025-05-27 22:17:25 +0530

    Update CricBet99-vs-Other.js

--- src/app/components/CricBet99-vs-Other/CricBet99-vs-Other.js
+++ src/app/components/CricBet99-vs-Other/CricBet99-vs-Other.js
@@ -91,7 +91,7 @@ export default function CricBet99Other() {
                 <div className="text-center anc-tab">
                 <a
                     className="elementor-button elementor-button-link elementor-size-sm elementor-animation-pulse-grow"
-                    href="#"
+                    href="https://wa.me/918441068489"
                     >
                     <span className="elementor-button-content-wrapper">
                         <span className="elementor-button-text">Get CricBet99 ID</span>
